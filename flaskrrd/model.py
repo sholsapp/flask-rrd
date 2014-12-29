19ca80abfe5d2da4e71f0f07f0929c03b9104d7a
Stephen Holsapple <sholsapp@gmail.com>
2014-12-29 12:51:07 -0800

    Add a model for RRD databases

--- flaskrrd/model.py
+++ flaskrrd/model.py
@@ -5,15 +5,28 @@ from sqlalchemy import Column, Integer, String
 db = SQLAlchemy()
 
 
-class Messages(db.Model):
-  """A database table for messages."""
-  __tablename__ = 'messages'
+class RRD(db.Model):
+  """A database table for RRD databases."""
+
+  __tablename__ = 'rrd'
+
   id = db.Column(db.Integer, primary_key=True)
-  message = db.Column(db.String(80))
+  name = db.Column(db.String(60))
+  cols_n = db.Column(db.Integer)
+  cols_desc = db.Column(db.String(60))
+  type = db.Column(db.String(60))
+
   def __repr__(self):
-    return 'Messages(%r)' % repr(self.message)
-  def __init__(self, message):
-    self.message = message
+    return 'RRD(%r, %r, %r)' % (
+      repr(self.name),
+      repr(self.cols_desc.split(',')),
+      repr(self.type))
+
+  def __init__(self, name, cols, type):
+    self.name = name
+    self.cols_n = len(cols)
+    self.cols_desc = ','.join(cols)
+    self.type = type
 
 
 def make_conn_str():
